8bec9f0e2da60b3bd9d8b12636f465e0c2a41ee7
Your Name <you@example.com>
2020-10-30 19:21:27 +0800

    个人中心功能完成

--- assets/js/user/user_avatar.js
+++ assets/js/user/user_avatar.js
@@ -11,4 +11,55 @@ $(function () {
 
     // 1.3 创建裁剪区域
     $image.cropper(options)
+
+    // 选择文件
+    $('#btnChooseImage').on('click', function () {
+        $('#file').click()
+    })
+
+    // 修改剪裁图片
+    var layer = layui.layer
+    $('#file').on('change', function (e) {
+        var file = e.target.files[0]
+        if (file == undefined) {
+            return layer.msg("请选择图片！")
+        }
+
+        var newImgURL = URL.createObjectURL(file)
+
+        $image
+            .cropper('destroy') // 销毁旧的裁剪区域
+            .attr('src', newImgURL) // 重新设置图片路径
+            .cropper(options) // 重新初始化裁剪区域
+    })
+
+
+    // 4.上传头像
+    $('#btnUpload').on('click', function () {
+        var dataURL = $image
+            .cropper('getCroppedCanvas', {
+                // 创建一个 Canvas 画布
+                width: 100,
+                height: 100
+            })
+            .toDataURL('image/png')
+        console.log(dataURL)
+        console.log(typeof dataURL)
+
+        $.ajax({
+            method: 'POST',
+            url: '/my/update/avatar',
+            data: {
+                avatar: dataURL
+            },
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('更换头像失败！')
+                }
+                layer.msg('更换头像成功！')
+                window.parent.getUserInfo()
+            }
+        })
+    })
+
 })
